7c1b4c5e9a6f4454a12b966c9590ed2b54f6b3c8
goatsejpg <pooplooklook@gmail.com>
2017-02-22 20:30:06 +0000

    Optimised
    FPS limit and new frame rendered only when needed

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,16 @@
 #include <SDL2/SDL_ttf.h>
 #include <iostream>
 #include <unistd.h>
+#include <time.h>
 
 #include "Structs.h"
 #include "Globals.h"
 #include "SDLFuncs.h"
 #include "Logic.h"
 #include "Buttons.h"
+
+const int FPSlimit = 60;
+const int FPSsleep = 1000000 / FPSlimit;
 
 int main (int argc, char* argv[]) {
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -36,6 +40,11 @@ int main (int argc, char* argv[]) {
 	bool firstCellDrawn;
 	bool PAUSED = true;
 	unsigned char count;
+
+	bool drawFrame = true;
+
+	struct timespec start, end;
+	unsigned int deltaTime; // micro seconds
 
 	Display display(Vec2(105,510), renderer, &SPEED, "DELAY", OpenSans, Clock);
 
@@ -67,7 +76,20 @@ int main (int argc, char* argv[]) {
 	buttons[0] = &minusSpeed; buttons[1] = &plusSpeed;
 	buttons[2] = &pauseButton; buttons[3] = &reset;
 
+	gameoflife::renderMatrix(renderer);
+
+	display.render(renderer);
+	for (auto B : buttons) {
+		B->render(renderer);
+	}	
+	SDL_RenderPresent(renderer);
+	SDL_RenderPresent(renderer);
+
 	while (playing) {
+
+		clock_gettime(CLOCK_MONOTONIC, &start);
+		drawFrame = false;
+
 		while (SDL_PollEvent(&event)) {
 			switch (event.type) {
 			case SDL_QUIT:
@@ -84,6 +106,7 @@ int main (int argc, char* argv[]) {
 					for (auto B : buttons) {
 						if (B->checkPress(mouseXY)) {
 							B->action();
+							drawFrame = true;
 							break;
 						}
 					}
@@ -93,6 +116,7 @@ int main (int argc, char* argv[]) {
 
 					if (mouseXY != Vec2(-1, -1)) {
 						draw = true;
+						drawFrame = true;
 					}
 					break;
 				}
@@ -125,6 +149,7 @@ int main (int argc, char* argv[]) {
 			gameoflife::updateNextMatrix();
 			gameoflife::nextGeneration();
 			count = 0;
+			drawFrame = true;
 		}
 
 		if (draw) {
@@ -132,20 +157,28 @@ int main (int argc, char* argv[]) {
 			if (temp != lastCellDrawn && firstCellDrawn == gameoflife::getCell(temp)) {
 				gameoflife::invertCell(temp);
 				MATRIXNEXT[temp.x][temp.y] = !firstCellDrawn;
+				drawFrame = true;
 			}
 			lastCellDrawn = temp;
 		}
 
-		gameoflife::renderMatrix(renderer);
+		if (drawFrame) {
+			gameoflife::renderMatrix(renderer);
 
-		display.render(renderer);
-		for (auto B : buttons) {
-			B->render(renderer);
+			display.render(renderer);
+			for (auto B : buttons) {
+				B->render(renderer);
+			}
+			SDL_RenderPresent(renderer);
 		}
 
 		count++;
 
-		SDL_RenderPresent(renderer);
+		clock_gettime(CLOCK_MONOTONIC, &end);
+		deltaTime = (end.tv_nsec - start.tv_nsec)/1000;
+		if (deltaTime < FPSsleep) {
+			usleep (FPSsleep - deltaTime);
+		}
 	}
 
 	return 0;
